5238528484a4dd083ee7e97b85e37eebbbf60927
Ridhwan Deshwal <ridhwandeshwal@gmail.com>
2026-07-21 01:32:53 +0530

    x4

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Sun Jul 19 18:26:54 2026
-__version__ = '1.2.0+d603878'
+# TIME: Mon Jul 20 17:04:29 2026
+__version__ = '1.2.0+a814c37'
 short_version = '1.2.0'
 version_info = (1, 2, 0)
